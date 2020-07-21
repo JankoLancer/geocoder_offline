112e0c94242830607fe47d1b73d95648cedf5d30
Janko Djuric <jdjuric@lioneight.com>
2020-07-21 09:41:34 +0200

    Change GNIS format when distance is 0

--- lib/src/LocationResult.dart
+++ lib/src/LocationResult.dart
@@ -1,5 +1,5 @@
-import '../geocoder_offline.dart';
 import './SearchData.dart';
+import '../geocoder_offline.dart';
 
 class LocationResult {
   /// Location data that was finded in search. Contains data from provided search file.
@@ -16,7 +16,11 @@ class LocationResult {
 
   LocationResult(this.location, this.distance, this.bearing, this.searchData);
 
+  /// Get LocationResult representation in GNIS format
   String get gnisFormat {
+    if (distance == 0) {
+      return '${location.featureName}, ${location.state}';
+    }
     return '${distance.toInt()}mi $bearing of ${location.featureName}, ${location.state} ';
   }
 }
