5710676bcc02e7357e87b5478b1a693ac6cc409e
J-P Nurmi <jpnurmi@gmail.com>
2022-03-23 14:47:51 +0100

    Add test for passing numMarkers to seach()

--- test/geocoder_offline_test.dart
+++ test/geocoder_offline_test.dart
@@ -48,4 +48,27 @@ void main() {
     expect(result.first.location.featureName, 'Chicago Loop');
     expect(result.first.gnisFormat.trim(), 'Chicago Loop, US');
   });
+
+  test('Test number of nearest result', () {
+    var geocoder = GeocodeData(
+        File(Directory.current.path + '/example/cities15000.txt')
+            .readAsStringSync(),
+        'name',
+        'country code',
+        'latitude',
+        'longitude',
+        fieldDelimiter: '\t',
+        eol: '\n');
+
+    var result = geocoder.search(41.881832, -87.623177, numMarkers: 2);
+    expect(result.length, 2);
+    expect(result.first.distance.toInt(), 1);
+    expect(result.first.bearing, 'SE');
+    expect(result.first.location.featureName, 'Near North Side');
+    expect(result.first.gnisFormat.trim(), '1mi SE of Near North Side, US');
+    expect(result.last.distance.toInt(), 0);
+    expect(result.last.bearing, 'E');
+    expect(result.last.location.featureName, 'Chicago Loop');
+    expect(result.last.gnisFormat.trim(), 'Chicago Loop, US');
+  });
 }
